7826be5d7fb44a47b9f81c6b65d0d745bb5f362e
Pandraghon <ryuunushi@gmail.com>
2018-02-13 10:39:41 +0100

    Add Download button

--- .github/build.sh
+++ .github/build.sh
@@ -4,7 +4,6 @@ git remote rm origin
 git remote add origin https://Pandraghon:${GITHUB_TOKEN}@github.com/gw2ocs/gw2icon.git
 git fetch
 git checkout -b build origin/build
-git rev-parse --abbrev-ref HEAD
 git clean -fdx
 rm -rf img
 rm -rf css
@@ -12,10 +11,7 @@ git checkout master -- build/*
 mv build/* .
 rm -rf build
 rm css/gw2icon.min.css.map
-git rev-parse --abbrev-ref HEAD
 git add .
-git rev-parse --abbrev-ref HEAD
 git commit -m "Update build $TRAVIS_COMMIT"
-git rev-parse --abbrev-ref HEAD
 git push origin HEAD:build
 git checkout master
